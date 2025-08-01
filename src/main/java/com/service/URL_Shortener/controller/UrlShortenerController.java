9f004f1a9a88a626828fd1c3d489ccc49e7d1f35
Aditya Yadav <adityayadav213@gmail.com>
2025-08-01 17:59:36 +0530

    Created rate limiter for both original and short urls

--- src/main/java/com/service/URL_Shortener/controller/UrlShortenerController.java
+++ src/main/java/com/service/URL_Shortener/controller/UrlShortenerController.java
@@ -2,6 +2,7 @@ package com.service.URL_Shortener.controller;
 
 import com.service.URL_Shortener.model.UrlRequest;
 import com.service.URL_Shortener.service.UrlShortenerService;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,12 +20,14 @@ public class UrlShortenerController {
     }
 
     @PostMapping("/shorten")
+    @RateLimiter(name = "shortenUrlLimiter")
     public String shorten(@RequestBody UrlRequest request){
 
         return service.shortenUrl(request.getOriginalUrl());
     }
 
     @GetMapping("/{shortCode}")
+    @RateLimiter(name = "redirectUrlLimiter")
     public ResponseEntity<?> redirect(@PathVariable String shortCode){
         String originalUrl = service.getOriginalUrl(shortCode);
         return ResponseEntity.status(HttpStatus.FOUND)
